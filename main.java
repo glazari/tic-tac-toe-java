b49474dce4c04002fd3e4696053d572761390384
Guilherme Lazari <guilherme.lazari@wildlifestudios.com>
2021-11-22 23:30:07 -0300

    Delete primes code

--- main.java
+++ main.java
@@ -23,25 +23,4 @@ class HelloWorld {
       }
     }
   }
-
-  public static void allPrimes(int num) {
-    for (int i = 0; i <= 100; i++) {
-      if (isPrime(i)) {
-        System.out.println(i);
-      }
-    }
-  }
-
-  public static boolean isPrime(int num) {
-    if (num <= 1) {
-      return false;
-    }
-
-    for (int i = 2; i * i <= num; i++) {
-      if (num % i == 0) {
-        return false;
-      }
-    }
-    return true;
-  }
 }
