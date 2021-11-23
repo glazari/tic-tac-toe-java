6ebfc893417f7c8fbd5bf751bfa9d2ccf9b94c13
Guilherme Lazari <guilherme.lazari@wildlifestudios.com>
2021-11-22 23:26:56 -0300

    Refactor print board

--- main.java
+++ main.java
@@ -1,16 +1,30 @@
 class HelloWorld {
 
   public static void main(String[] args) {
+    char[][] board = new char[3][3];
+    board[0][0] = 'x';
+    System.out.println(board[0][0] == 'x');
+    System.out.println(board[0][1] == ' ');
+    System.out.println(board[0][1] == ' ');
     printBoard();
   }
 
   public static void printBoard() {
-    System.out.println(" x | o |   ");
-    System.out.println("___|___|___");
-    System.out.println(" x | o |   ");
-    System.out.println("___|___|___");
-    System.out.println(" x | o |   ");
-    System.out.println("   |   |   ");
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        System.out.print(" x ");
+        if (j < 2) {
+          System.out.print("|");
+        } else {
+          System.out.println("");
+        }
+      }
+      if (i < 2) {
+        System.out.println("___|___|___");
+      } else {
+        System.out.println("   |   |   ");
+      }
+    }
   }
 
   public static void allPrimes(int num) {
