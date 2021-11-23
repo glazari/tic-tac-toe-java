a3d2ad9feb09b05c17a414f51b78effcaf00b8ba
Guilherme Lazari <guilherme.lazari@wildlifestudios.com>
2021-11-22 23:38:02 -0300

    Add Board class

--- main.java
+++ main.java
@@ -1,12 +1,39 @@
 class HelloWorld {
 
   public static void main(String[] args) {
-    char[][] board = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
-    board[0][0] = 'x';
-    printBoard(board);
+    Board board = new Board();
+    board.print();
   }
 
   public static void printBoard(char[][] board) {
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        System.out.print(" " + board[i][j] + " ");
+        if (j < 2) {
+          System.out.print("|");
+        } else {
+          System.out.println("");
+        }
+      }
+      if (i < 2) {
+        System.out.println("___|___|___");
+      } else {
+        System.out.println("   |   |   ");
+      }
+    }
+  }
+}
+
+class Board {
+  char[][] board;
+
+  public Board() {
+    char[][] b = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+    board = b;
+    board[0][0] = 'x';
+  }
+
+  public void print() {
     for (int i = 0; i < 3; i++) {
       for (int j = 0; j < 3; j++) {
         System.out.print(" " + board[i][j] + " ");
