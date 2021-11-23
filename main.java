5315f9cdba46440da7a926c6f0e6b32f8f75da9d
Guilherme Lazari <guilherme.lazari@wildlifestudios.com>
2021-11-22 23:45:32 -0300

    Remove old print method

--- main.java
+++ main.java
@@ -4,24 +4,6 @@ class HelloWorld {
     Board board = new Board();
     board.print();
   }
-
-  public static void printBoard(char[][] board) {
-    for (int i = 0; i < 3; i++) {
-      for (int j = 0; j < 3; j++) {
-        System.out.print(" " + board[i][j] + " ");
-        if (j < 2) {
-          System.out.print("|");
-        } else {
-          System.out.println("");
-        }
-      }
-      if (i < 2) {
-        System.out.println("___|___|___");
-      } else {
-        System.out.println("   |   |   ");
-      }
-    }
-  }
 }
 
 class Board {
